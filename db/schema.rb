933b72dd6fa8a09654dde948c2106ffff2e12057
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-06-01 16:28:06 +0300

    migrations added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_31_070748) do
+ActiveRecord::Schema.define(version: 2021_05_31_145623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,14 +24,42 @@ ActiveRecord::Schema.define(version: 2021_05_31_070748) do
     t.bigint "group_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["group_id"], name: "index_contacts_on_group_id"
+    t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "contacts", "groups"
+  add_foreign_key "contacts", "users"
+  add_foreign_key "groups", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,48 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
-
-Group.destroy_all
 Contact.destroy_all
-group_ids = []
-group_ids << Group.create(name: 'Family').id
-group_ids << Group.create(name: 'Friends').id
-group_ids << Group.create(name: 'Colleagues').id
+Group.destroy_all
+User.destroy_all
+
+user_ids = []
+user_ids << User.create(name: "John Doe", email: "johndoe@test.com", password: "secret").id
+user_ids << User.create(name: "Jane Roe", email: "janeroe@test.com", password: "secret").id
+
+p "2 users created"
+
+group_ids = { user_ids[0] => [], user_ids[1] => [] }
+
+group_ids[user_ids[0]] << Group.create(name: "Client", user_id: user_ids[0]).id
+group_ids[user_ids[0]] << Group.create(name: "Friend", user_id: user_ids[0]).id
+group_ids[user_ids[1]] << Group.create(name: "Family", user_id: user_ids[1]).id
+group_ids[user_ids[1]] << Group.create(name: "Company", user_id: user_ids[1]).id
+
+p "#{group_ids.count} group created"
+
 group_count = group_ids.length
+number_of_contacts = 60
 contacts = []
 
-50.times do |contact|
-  new_contact = {
-    name: Faker::Name.name,
-    email: Faker::Internet.email,
-    company: Faker::Company.name,
-    phone: Faker::PhoneNumber.cell_phone,
-    address: "#{Faker::Address.street_address} #{Faker::Address.zip} #{Faker::Address.city}",
-    group_id: group_ids[Random.rand(0...group_count)]
+number_of_contacts.times do |i|
+    user_id = user_ids[Random.rand(0...2)]
 
-  }
-  contacts.push(new_contact)
+    new_contact = {
+        name: Faker::Name.name,
+        email: Faker::Internet.email,
+        company: Faker::Company.name,
+        phone: Faker::PhoneNumber.cell_phone,
+        address: "#{Faker::Address.street_address} #{Faker::Address.zip} #{Faker::Address.city}",
+        group_id: group_ids[user_id][Random.rand(0...group_count)],
+        user_id: user_id
+    }
+    contacts.push(new_contact)
 end
+
 Contact.create(contacts)
+
+p "#{number_of_contacts} contacts created"
