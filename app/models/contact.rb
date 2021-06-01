b92a1087e6cadda268c8daf70d45ca8fe58bc79c
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-06-01 16:18:13 +0300

    models completed

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,39 @@
 class Contact < ApplicationRecord
   belongs_to :group
+  belongs_to :user
+
+  paginates_per 10
+
+  validates :name, :email, :group_id, presence: true
+  validates :name, length: { minimum: 2 }
+
+  has_attached_file :avatar, styles: { medium: "150x150>", thumb: "100x100>" }, default_url: "/images/:style/default.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+  def gravatar
+    hash = Digest::MD5.hexdigest(email.downcase)
+    "https://www.gravatar.com/avatar/#{hash}"
+  end
+
+  scope :search, -> (term) do
+      where('LOWER(name) LIKE :term or LOWER(company) LIKE :term or LOWER(email) LIKE :term', term: "%#{term.downcase}%") if term.present?
+  end
+
+  # def self.search(term)
+  #     if term && !term.empty?
+  #         where('name LIKE ?', "%#{term}%")
+  #     else
+  #         all
+  #     end
+  # end
+
+  scope :by_group, -> (group_id) { where(group_id: group_id) if group_id.present? }
+
+  # def self.by_group(group_id)
+  #     if group_id && !group_id.empty?
+  #         where(group_id: group_id)
+  #     else
+  #         all
+  #     end
+  # end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,7 @@
 class Group < ApplicationRecord
-  has_many :contacts
+    has_many :contacts
+    belongs_to :user
+
+    validates :name, presence: true
+    validates :name, uniqueness: true
 end
